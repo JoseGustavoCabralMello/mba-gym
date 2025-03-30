2dd6b63302e627ff15c186c507806b9b6b1dfe7c
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-30 10:40:33 -0300

    Estrutura da aplicação: Fonte da aplicação

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  Roboto_400Regular as Roboto400Regular,
+  Roboto_700Bold as Roboto700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+import { Text, View } from 'react-native'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto700Bold, Roboto400Regular })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {fontsLoaded ? <Text>Home</Text> : <View />}
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
