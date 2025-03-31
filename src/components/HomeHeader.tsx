726cb6b8c2d831ddb36dbb593281c5467382d2f5
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-31 20:52:31 -0300

    Finalizando as interfaces: Botão Logout

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -6,7 +6,7 @@ export function HomeHeader() {
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
             <UserPhoto source={{ uri: 'https://github.com/arthurrios.png'}} w='$16' h='$16' alt='Imagem do usuário' />
-      <VStack>
+      <VStack  flex={1}>
         <Text color="$gray100" fontSize="$sm">
           Olá
         </Text>
