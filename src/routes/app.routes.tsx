de9e83d2ec1945226482faf405636b51a5f392c9
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-31 19:46:36 -0300

    Rotas Privadas: Alterando os ícones da Bottom Navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -7,6 +7,11 @@ import { Home } from '@screens/Home'
 import { Exercise } from '@screens/Exercise'
 import { History } from '@screens/History'
 import { Profile } from '@screens/Profile'
+import { gluestackUIConfig } from '../../config/gluestack-ui.config'
+
+import HomeSvg from '@assets/home.svg'
+import HistorySvg from '@assets/history.svg'
+import ProfileSvg from '@assets/profile.svg'
 
 type AppRoutes = {
   home: undefined
@@ -20,13 +25,38 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
 export function AppRoutes() {
+  const { tokens } = gluestackUIConfig
+  const iconSize = tokens.space['6']
+
   return (
     <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
-      <Screen name="home" component={Home} />
-
-      <Screen name="history" component={History} />
-
-      <Screen name="profile" component={Profile} />
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="history"
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
 
       <Screen name="exercise" component={Exercise} />
     </Navigator>
