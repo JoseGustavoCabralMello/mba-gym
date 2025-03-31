4ce3ed6860fc6c0f593fa090c10cd091d6402945
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-31 19:50:54 -0300

    Rotas Privadas: Estilizando a Bottom Navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,11 +3,14 @@ import {
   createBottomTabNavigator,
 } from '@react-navigation/bottom-tabs'
 
+import { gluestackUIConfig } from '../../config/gluestack-ui.config'
+
+import { Platform } from 'react-native'
+
 import { Home } from '@screens/Home'
 import { Exercise } from '@screens/Exercise'
 import { History } from '@screens/History'
 import { Profile } from '@screens/Profile'
-import { gluestackUIConfig } from '../../config/gluestack-ui.config'
 
 import HomeSvg from '@assets/home.svg'
 import HistorySvg from '@assets/history.svg'
@@ -35,6 +38,13 @@ export function AppRoutes() {
         tabBarShowLabel: false,
         tabBarActiveTintColor: tokens.colors.green500,
         tabBarInactiveTintColor: tokens.colors.gray200,
+        tabBarStyle: {
+          backgroundColor: tokens.colors.gray600,
+          borderTopWidth: 0,
+          height: Platform.OS === 'android' ? 'auto' : 96,
+          paddingBottom: tokens.space['10'],
+          paddingTop: tokens.space['6'],
+        },
       }}
     >
       <Screen
@@ -65,7 +75,11 @@ export function AppRoutes() {
         }}
       />
 
-      <Screen name="exercise" component={Exercise} />
+      <Screen
+        name="exercise"
+        component={Exercise}
+        options={{ tabBarButton: () => null }}
+      />
     </Navigator>
   )
 }
