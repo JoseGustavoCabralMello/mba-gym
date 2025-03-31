147e95cc8dc1a83aba4952755d72dc3f35b469e2
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-31 07:17:45 -0300

    Rotas Públicas: Criando o contexto de navegação

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { StatusBar } from 'react-native'
 import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from '@components/Loading'
-import { SignUp } from '@screens/SignUp'
+import { Routes } from '@routes/index'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto700Bold, Roboto400Regular })
@@ -20,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </GluestackUIProvider>
   )
 }
