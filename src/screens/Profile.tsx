886029bc267341fbafd13fe6e174305dfbd4ecb3
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-04-02 08:06:10 -0300

    Galeria de Fotos: Restringindo tamanho da foto

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -3,7 +3,7 @@ import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
 import { Center, Heading, Text, VStack } from '@gluestack-ui/themed'
-import { ScrollView, TouchableOpacity } from 'react-native'
+import { Alert, ScrollView, TouchableOpacity } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import { useState } from 'react'
 import * as FileSystem from 'expo-file-system'
@@ -14,81 +14,91 @@ export function Profile() {
   )
 
   async function handleUserPhotoSelect() {
-    const photoSelected = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality: 1,
-      aspect: [4, 4],
-      allowsEditing: true,
-    })
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
 
-    if (photoSelected.canceled) {
-      return
-    }
-
-    const photoUri = photoSelected.assets[0].uri
-
-    if (photoUri) {
-      const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as {
-        size: number
+      if (photoSelected.canceled) {
+        return
       }
 
-      setUserPhoto(photoSelected.assets[0].uri)
+      const photoUri = photoSelected.assets[0].uri
+
+      if (photoUri) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as {
+          size: number
+        }
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return Alert.alert(
+            'Essa imagem é muito grande. Escolha uma de até 5MB',
+          )
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+    } catch (error) {
+      console.log(error)
     }
-  }
 
-  return (
-    <VStack flex={1}>
-      <ScreenHeader title="Perfil" />
+    return (
+      <VStack flex={1}>
+        <ScreenHeader title="Perfil" />
 
-      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
-        <Center mt="$6" px="$10">
-          <UserPhoto
-            source={{ uri: userPhoto }}
-            size="xl"
-            alt="Imagem do usuário"
-          />
-
-          <TouchableOpacity onPress={handleUserPhotoSelect}>
-            <Text
-              color="$green500"
-              fontFamily="$heading"
-              fontSize="$md"
-              mt="$2"
-              mb="$8"
-            >
-              Alterar Foto
-            </Text>
-          </TouchableOpacity>
-
-          <Center w="$full" gap="$4">
-            <Input placeholder="Nome" bg="$gray600" />
-            <Input value="arthur@email.com" bg="$gray600" isReadOnly />
-          </Center>
-
-          <Heading
-            alignSelf="flex-start"
-            fontFamily="$heading"
-            color="$gray200"
-            fontSize="$md"
-            mt="$12"
-            mb="$2"
-          >
-            Alterar senha
-          </Heading>
-
-          <Center w="$full" gap="$4">
-            <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
-            <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
-            <Input
-              placeholder="Confirme a nova senha"
-              bg="$gray600"
-              secureTextEntry
+        <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+          <Center mt="$6" px="$10">
+            <UserPhoto
+              source={{ uri: userPhoto }}
+              size="xl"
+              alt="Imagem do usuário"
             />
 
-            <Button title="Atualizar" />
+            <TouchableOpacity onPress={handleUserPhotoSelect}>
+              <Text
+                color="$green500"
+                fontFamily="$heading"
+                fontSize="$md"
+                mt="$2"
+                mb="$8"
+              >
+                Alterar Foto
+              </Text>
+            </TouchableOpacity>
+
+            <Center w="$full" gap="$4">
+              <Input placeholder="Nome" bg="$gray600" />
+              <Input value="arthur@email.com" bg="$gray600" isReadOnly />
+            </Center>
+
+            <Heading
+              alignSelf="flex-start"
+              fontFamily="$heading"
+              color="$gray200"
+              fontSize="$md"
+              mt="$12"
+              mb="$2"
+            >
+              Alterar senha
+            </Heading>
+
+            <Center w="$full" gap="$4">
+              <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+              <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+              <Input
+                placeholder="Confirme a nova senha"
+                bg="$gray600"
+                secureTextEntry
+              />
+
+              <Button title="Atualizar" />
+            </Center>
           </Center>
-        </Center>
-      </ScrollView>
-    </VStack>
-  )
+        </ScrollView>
+      </VStack>
+    )
+  }
 }
