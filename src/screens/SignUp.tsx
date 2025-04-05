3ac00c7aece92e2525326e44c395306ec38c28ee
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-04-05 12:57:37 -0300

    HTTP Client - Utilizando Axios

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -13,6 +13,8 @@ import { Controller, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 
+import { api } from '@services/api'
+
 import Logo from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 
@@ -55,16 +57,8 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await fetch('http://localhost:3333/users', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, password }),
-    })
-    const data = await response.json()
-    console.log(data)
+    const response = await api.post('/users', { name, email, password })
+    console.log(response.data)
   }
 
   return (
