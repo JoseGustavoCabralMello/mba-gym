0e490bd7218a4133c7d5a381a8019f5d3d452a15
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-04-08 07:54:02 -0300

    Contextos no React Native - Criando conta e logando usuário

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -23,6 +23,8 @@ import { api } from '@services/api'
 import axios from 'axios'
 import { Alert } from 'react-native'
 import { AppError } from '@utils/AppError'
+import { useState } from 'react'
+import { useAuth } from '@hooks/useAuth'
 
 type FormDataProps = {
   name: string
@@ -45,7 +47,10 @@ const signUpSchema = yup.object({
 })
 
 export function SignUp() {
+  const [isLoading, setIsLoading] = useState(false)
+
   const toast = useToast()
+  const { singIn } = useAuth()
 
   const {
     control,
@@ -63,9 +68,13 @@ export function SignUp() {
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
     try {
-      const response = await api.post('/users', { name, email, password })
-      console.log(response.data)
+      setIsLoading(true)
+
+      await api.post('/users', { name, email, password })
+      await singIn(email, password)
     } catch (error) {
+      setIsLoading(false)
+
       const isAppError = error instanceof AppError
 
       const title = isAppError
@@ -176,6 +185,7 @@ export function SignUp() {
             variant="outline"
             mt="$12"
             onPress={handleGoBack}
+            isLoading={isLoading}
           />
         </VStack>
       </VStack>
