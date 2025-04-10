4854827ff4f5967d2a1f2d062ff2d70baface42f
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-04-10 16:27:11 -0300

    Consumindo o Back-end - Carregando dados do usuário no profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -8,6 +8,16 @@ import * as ImagePicker from 'expo-image-picker'
 import { useState } from 'react'
 import * as FileSystem from 'expo-file-system'
 import { ToastMessage } from '@components/ToastMessage'
+import { useAuth } from '@hooks/useAuth'
+import { Controller, useForm } from 'react-hook-form'
+
+type FormDataProps = {
+  name: string
+  email: string
+  password: string
+  oldPassword: string
+  newPassword: string
+}
 
 export function Profile() {
   const [userPhoto, setUserPhoto] = useState(
@@ -15,6 +25,14 @@ export function Profile() {
   )
 
   const toast = useToast()
+
+  const { user } = useAuth()
+  const { control } = useForm<FormDataProps>({
+    defaultValues: {
+      name: user.name,
+      email: user.email,
+    },
+  })
 
   async function handleUserPhotoSelect() {
     try {
@@ -82,8 +100,31 @@ export function Profile() {
             </TouchableOpacity>
 
             <Center w="$full" gap="$4">
-              <Input placeholder="Nome" bg="$gray600" />
-              <Input value="arthur@email.com" bg="$gray600" isReadOnly />
+              <Controller
+                control={control}
+                name="name"
+                render={({ field: { value, onChange } }) => (
+                  <Input
+                    bg="gray.600"
+                    placeholder="Nome"
+                    onChangeText={onChange}
+                    value={value}
+                  />
+                )}
+              />
+              <Controller
+                control={control}
+                name="email"
+                render={({ field: { value, onChange } }) => (
+                  <Input
+                    bg="gray.600"
+                    placeholder="E-mail"
+                    // isDisabled
+                    onChangeText={onChange}
+                    value={value}
+                  />
+                )}
+              />
             </Center>
 
             <Heading
