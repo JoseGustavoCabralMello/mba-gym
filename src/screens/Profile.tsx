bef36dfe3310377c0ae9aa81d422c8b03a6451f8
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-04-11 07:22:48 -0300

    Consumindo o Back-end - Atualizando usuário no back-end

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -12,6 +12,8 @@ import { useAuth } from '@hooks/useAuth'
 import { Controller, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { api } from '@services/api'
+import { AppError } from '@utils/AppError'
 
 type FormDataProps = {
   name: string
@@ -44,6 +46,7 @@ const profileSchema = yup.object({
 })
 
 export function Profile() {
+  const [isUpdating, setIsUpdating] = useState(false)
   const [userPhoto, setUserPhoto] = useState(
     'https://github.com/arthurrios.png',
   )
@@ -104,7 +107,29 @@ export function Profile() {
     }
 
     async function handleProfileUpdate(data: FormDataProps) {
-      console.log(data)
+      try {
+        setIsUpdating(true)
+        await api.put('/users', data)
+
+        toast.show({
+          title: 'Perfil atualizado com sucesso!',
+          placement: 'top',
+          bgColor: 'green.500',
+        })
+      } catch (error) {
+        const isAppError = error instanceof AppError
+        const title = isAppError
+          ? error.message
+          : 'Não foi possível atualizar os dados. Tente novamente mais tarde.'
+
+        toast.show({
+          title,
+          placement: 'top',
+          bgColor: 'red.500',
+        })
+      } finally {
+        setIsUpdating(false)
+      }
     }
 
     return (
@@ -215,6 +240,7 @@ export function Profile() {
                 title="Atualizar"
                 mt={4}
                 onPress={handleSubmit(handleProfileUpdate)}
+                isLoading={isUpdating}
               />
             </Center>
           </Center>
