1a9d3063924bac5514afefbb634c1ac8f0fb6238
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-04-11 22:02:21 -0300

    Refresh Token - Armazenando o Refresh Token

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -39,11 +39,15 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     setUser(userData)
   }
 
-  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
+  async function storageUserAndTokenSave(
+    userData: UserDTO,
+    token: string,
+    refresh_token: string,
+  ) {
     try {
       setIsLoadingUserStorageData(true)
       await storageUserSave(userData)
-      await storageAuthTokenSave(token)
+      await storageAuthTokenSave({ token, refresh_token })
       // eslint-disable-next-line no-useless-catch
     } catch (error) {
       throw error
@@ -57,8 +61,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     try {
       const { data } = await api.post('/sessions', { email, password })
 
-      if (data.user && data.token) {
-        await storageUserAndTokenSave(data.user, data.token)
+      if (data.user && data.token && data.refresh_token) {
+        await storageUserAndTokenSave(data.user, data.token, data.refresh_token)
         userAndTokenUpdate(data.user, data.token)
       }
       // eslint-disable-next-line no-useless-catch
@@ -98,8 +102,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setIsLoadingUserStorageData(true)
 
       const userLogged = await storageUserGet()
-      const token = await storageAuthTokenGet()
-
+      const { token } = await storageAuthTokenGet()
       if (token && userLogged) {
         userAndTokenUpdate(userLogged, token)
       }
